26f7d119dd38b41a1d217a3e847c2c25aebdb5d0
Dmitry <mitya674614@gmail.com>
2024-11-18 17:01:15 +0300

    INTR-FP-Made preamble

---
+++ force-app/main/default/lwc/preamble/preamble.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import Image_Url from '@salesforce/resourceUrl/carImages'
+
+export default class Preamble extends LightningElement {
+    preambleImage = Image_Url + '/LadaVestaSedan.png';
+}
