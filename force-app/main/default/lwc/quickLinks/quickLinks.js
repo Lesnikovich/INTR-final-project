99737531a4c0a9c792a14f1cc7e6e6886ed4a387
Dmitry <mitya674614@gmail.com>
2024-11-19 17:01:23 +0300

    INTR-FP-Made the site navigation

--- force-app/main/default/lwc/quickLinks/quickLinks.js
+++ force-app/main/default/lwc/quickLinks/quickLinks.js
@@ -1,7 +1,8 @@
 import { LightningElement } from 'lwc';
 import Image_Url from '@salesforce/resourceUrl/carImages'
+import {NavigationMixin} from 'lightning/navigation';
 
-export default class QuickLinks extends LightningElement {
+export default class QuickLinks extends NavigationMixin(LightningElement) {
     data = [
         {
             id: 1,
@@ -23,5 +24,34 @@ export default class QuickLinks extends LightningElement {
             image: Image_Url + '/LadaVestaSedan.png',
             text: 'Модельный ряд',
         },
-    ]
+    ];
+
+    handleClick(event){
+        let selectedCard = Number(event.currentTarget.dataset.id);
+        console.log('selectedCard:'+JSON.stringify(selectedCard));
+
+        switch (selectedCard) {
+            case 1:
+                this.navigateToPages('models__c');
+            break;
+            case 2:
+                this.navigateToPages('centers__c');
+            break;
+            case 3:
+                this.navigateToPages('testdrive__c');
+            break;
+            default:
+                console.error("Error navigating to the page from Quick Links.");
+          }
+    }
+
+    navigateToPages(pageApiName){
+        console.log('pageApiName in navigation function:'+JSON.stringify(pageApiName))
+        this[NavigationMixin.Navigate]({
+            type:'comm__namedPage',
+            attributes:{
+                name:pageApiName
+            }
+        })
+    }
 }
